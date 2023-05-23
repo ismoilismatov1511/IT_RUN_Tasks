8b72f748e971b26606387b99345a383eadc51c52
ismoilismatov1511 <ismoilismatov1511@gmail.com>
2023-05-23 16:36:18 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Программа для решения квадратных уравнений ax^2 + bx + c = 0");
+
+        Console.Write("Введите значение a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите значение c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0)
+        {
+            double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            Console.WriteLine("Уравнение имеет два корня:");
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+        }
+        else if (discriminant == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine("Уравнение имеет один корень:");
+            Console.WriteLine("x = " + x);
+        }
+        else
+        {
+            Console.WriteLine("Уравнение не имеет действительных корней.");
+        }
+
+        Console.ReadLine();
+    }
+}
