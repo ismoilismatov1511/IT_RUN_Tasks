36919d38811131208ac4d92f4f83f63e8cfe89cd
ismoilismatov1511 <ismoilismatov1511@gmail.com>
2023-06-07 13:39:52 +0500

    day 3 task 2

---
+++ day3/task2/Application.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedAndUnmanaged
+{
+    public class Application : IDisposable
+    {
+        StreamWriter StreamWriter;
+        public Application()
+        {
+            StreamWriter = new StreamWriter("store.txt", true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        bool disposed;
+        protected void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                //Clean up managed code
+                StreamWriter.Dispose();
+            }
+            //clean up unmanaged code
+            disposed = true;
+        }
+
+        ~Application()
+        {
+            Dispose(false);
+        }
+
+        public void WriteProduct(string name, int count)
+        {
+            StreamWriter.WriteLine($"{name} {count}");
+            StreamWriter.Flush();
+        }
+    }
+}
---
+++ day3/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ManagedAndUnmanaged;
+using static System.Net.Mime.MediaTypeNames;
+
+while (true)
+{
+    Console.WriteLine("Choose operation:");
+
+    var operation = Console.ReadLine();
+    if (operation == "enter")
+    {
+        Console.WriteLine("Name:");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Count:");
+        var count = int.Parse(Console.ReadLine());
+
+        using (Application application = new Application())
+        {
+            application.WriteProduct(name, count);
+        }
+    }
+}
+
+class test
+{
+
+}
